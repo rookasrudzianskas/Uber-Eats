e6806d564e04072ec5262d9e237293343e6df9e4
Rokas <rookas.rudzenskas@gmail.com>
2021-10-05 11:32:42 +0300

    Rokas

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import {View, Text} from "react-native";
 import {GooglePlacesAutocomplete} from "react-native-google-places-autocomplete";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import AntDesign from "react-native-vector-icons/AntDesign";
 
 const SearchBar = () => {
     return (
@@ -30,6 +32,20 @@ const SearchBar = () => {
                         marginRight: 10,
                     },
                 }}
+                renderLeftButton={() => (
+                    <View style={{marginLeft: 10}}>
+                        <Ionicons name="location-sharp" size={24} color={'black'} />
+                    </View>
+                    )
+                }
+
+                renderRightButton={()  => (
+                    <View style={{flexDirection: 'row'}}>
+                        <AntDesign name="clockcircle" size={11} color="black" />
+                        <Text style={{}}>Search</Text>
+
+                    </View>
+                )}
 
             />
         </View>
