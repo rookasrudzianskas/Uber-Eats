47efd1c67c174fb6c91c65d64cd78ee22fa05dc7
Rokas <rookas.rudzenskas@gmail.com>
2021-10-05 15:26:18 +0300

    Rokas

---
+++ screens/RestaurantDetail.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from "react-native";
+
+const RestaurantDetail = () => {
+    return (
+        <View style={{}}>
+
+        </View>
+    );
+};
+
+export default RestaurantDetail;
