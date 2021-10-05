48af6f4ea62cd3ddd8aeacbe8e589393462754e7
Rokas <rookas.rudzenskas@gmail.com>
2021-10-05 17:45:54 +0300

    ROkas

---
+++ components/restaurantDetail/ViewCart.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from "react-native";
+
+const ViewCart = () => {
+    return (
+        <View style={{}}>
+
+        </View>
+    );
+};
+
+export default ViewCart;
--- screens/RestaurantDetail.js
+++ screens/RestaurantDetail.js
@@ -3,13 +3,15 @@ import {View, Text} from "react-native";
 import {Divider} from "react-native-elements";
 import About from "../components/restaurantDetail/About";
 import MenuItems from "../components/restaurantDetail/MenuItems";
+import ViewCart from "../components/restaurantDetail/ViewCart";
 
-const RestaurantDetail = ({route}) => {
+const RestaurantDetail = ({route, navigation}) => {
     return (
         <View style={{}}>
             <About route={route} />
             <Divider width={1.8} style={{marginVertical: 20}} />
             <MenuItems />
+            <ViewCart navigation={navigation} restaurantName={route.params.name} />
         </View>
     );
 };
