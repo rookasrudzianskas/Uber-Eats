c0a19b1807ae565813e8c634c58b41493c7e1ce8
Rokas <rookas.rudzenskas@gmail.com>
2021-10-05 11:20:45 +0300

    Rokas

---
+++ components/SearchBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from "react-native";
+
+const SearchBar = () => {
+    return (
+        <View>
+
+        </View>
+    );
+};
+
+export default SearchBar;
--- screens/Home.js
+++ screens/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import {View, Text, SafeAreaView} from "react-native";
 import HeaderTabs from "../components/HeaderTabs";
+import SearchBar from "../components/SearchBar";
 
 const Home = () => {
     return (
         <SafeAreaView style={{backgroundColor: '#eee', flexGrow: 1}}>
             <View style={{backgroundColor: 'white', padding: 15}} >
                 <HeaderTabs />
+                <SearchBar />
             </View>
         </SafeAreaView>
     );
