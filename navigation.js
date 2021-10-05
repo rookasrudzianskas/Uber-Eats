b6c59e174eb836b720dbb7c378f18969b3d1fbd3
Rokas <rookas.rudzenskas@gmail.com>
2021-10-05 17:07:10 +0300

    Rokas

--- navigation.js
+++ navigation.js
@@ -1,20 +1,20 @@
-// import React from "react";
-// import { NavigationContainer } from "@react-navigation/native";
-// import Home from "./screens/Home";
-// import createStackNavigator from "react-native-screens/createNativeStackNavigator";
-//
-// export default function RootNavigation() {
-//     const Stack = createStackNavigator();
-//
-//     const screenOptions = {
-//         headerShown: false,
-//     };
-//
-//     return (
-//             <NavigationContainer>
-//                 <Stack.Navigator initialRouteName="Home" screenOptions={screenOptions}>
-//                     <Stack.Screen name="Home" component={Home} />
-//                 </Stack.Navigator>
-//             </NavigationContainer>
-//     );
-// }
+import React from "react";
+import Home from "./screens/Home";
+import {createStackNavigator} from "@react-navigation/stack";
+import {NavigationContainer} from "@react-navigation/native";
+
+export default function RootNavigation() {
+    const Stack = createStackNavigator();
+
+    const screenOptions = {
+        headerShown: false,
+    };
+
+    return (
+            <NavigationContainer>
+                <Stack.Navigator initialRouteName="Home" screenOptions={screenOptions}>
+                    <Stack.Screen name="Home" component={Home} />
+                </Stack.Navigator>
+            </NavigationContainer>
+    );
+}
