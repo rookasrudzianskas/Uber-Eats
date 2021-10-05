d23f1516862175763e9dc456821d2386dbedd22f
Rokas <rookas.rudzenskas@gmail.com>
2021-10-05 17:18:54 +0300

    Rokas

--- screens/Home.js
+++ screens/Home.js
@@ -9,7 +9,7 @@ import BottomTabs from "../components/home/BottomTabs";
 
 const YELP_API_KEY = 'mwmvzvgLFJAd6HbU0PNNepE3D8p18HFrNgv7Lks87zZgch52en03pB68hTVu3T2NCER7Vc7Yp6IvrWn6T0qIlEFv_oVD7lk0gnAtlqt357Uc6SSDn2y6EHMReCxcYXYx';
 
-const Home = () => {
+const Home = ({navigation}) => {
 
     const [restaurantData, setRestaurantData] = useState(localRestaurants);
     const [city, setCity] = useState('San Francisco');
@@ -46,7 +46,7 @@ const Home = () => {
             </View>
             <ScrollView showsVerticalScrollIndicator={false} style={{marginBottom: 120}}>
                 <Categories />
-                <RestaurantItems restaurantData={restaurantData} />
+                <RestaurantItems restaurantData={restaurantData} navigation={navigation} />
                 {/*<View style={{position: 'absolute', bottom: 0}}>*/}
                 <Divider width={1} />
                     <BottomTabs />
