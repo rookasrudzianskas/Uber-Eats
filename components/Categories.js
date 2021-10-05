a2f470de344af03847f42ad20a927ba343f0f6a3
Rokas <rookas.rudzenskas@gmail.com>
2021-10-05 11:56:39 +0300

    Rokas

--- components/Categories.js
+++ components/Categories.js
@@ -1,10 +1,41 @@
 import React from 'react';
 import {View, Text} from "react-native";
 
+const items = [
+    {
+        image: require("../assets/images/shopping-bag.png"),
+        text: "Pick-up",
+    },
+    {
+        image: require("../assets/images/soft-drink.png"),
+        text: "Soft Drinks",
+    },
+    {
+        image: require("../assets/images/bread.png"),
+        text: "Bakery Items",
+    },
+    {
+        image: require("../assets/images/fast-food.png"),
+        text: "Fast Foods",
+    },
+    {
+        image: require("../assets/images/deals.png"),
+        text: "Deals",
+    },
+    {
+        image: require("../assets/images/coffee.png"),
+        text: "Coffee & Tea",
+    },
+    {
+        image: require("../assets/images/desserts.png"),
+        text: "Desserts",
+    },
+];
+
 const Categories = () => {
     return (
         <View style={{}}>
-
+            <Text>Rokas</Text>
         </View>
     );
 };
