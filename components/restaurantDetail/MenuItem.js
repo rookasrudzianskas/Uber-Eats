832d5b20de35ce766a33aa64d7d3e2dcd8955da7
Rokas <rookas.rudzenskas@gmail.com>
2021-10-05 15:45:59 +0300

    Rokas

---
+++ components/restaurantDetail/MenuItem.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import {View} from "react-native";
+
+const foods = [
+    {
+        title: 'Lasagna',
+        description: 'Hello this is an awesome pizza, oh hey, lasagna',
+        price: '$14.53',
+        image: 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg'
+    },
+    {
+        title: "sea",
+        description: 'Hello this is an awesome pizza, oh hey, lasagna',
+        price: '$14.53',
+        image: 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg'
+    },
+    {
+        title: "contain",
+        description: 'Hello this is an awesome pizza, oh hey, lasagna',
+        price: '$14.53',
+        image: 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg'
+    },
+    {
+        title: "sit",
+        description: "disappearance",
+        price: '$14.53',
+        image: 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg'
+    },
+    {
+        title: "smooth",
+        description: 'Hello this is an awesome pizza, oh hey, lasagna',
+        price: '$14.53',
+        image: 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg'
+    },
+    {
+        title: "spot",
+        description: 'Hello this is an awesome pizza, oh hey, lasagna',
+        price: '$14.53',
+        image: 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg'
+    },
+]
+
+const MenuItem = () => {
+    return (
+        <View>
+
+        </View>
+    );
+};
+
+export default MenuItem;
