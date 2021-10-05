ceb5267522ad7de736551acfdcb42ee8d1b7964d
Rokas <rookas.rudzenskas@gmail.com>
2021-10-05 15:30:11 +0300

    Rokas

--- App.js
+++ App.js
@@ -2,13 +2,15 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Home from "./screens/Home";
+import RestaurantDetail from "./screens/RestaurantDetail";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Home />
-      <StatusBar style="auto" />
-    </View>
+      <>
+          {/*<Home />*/}
+          <RestaurantDetail />
+          <StatusBar style="auto" />
+      </>
   );
 }
 
--- screens/RestaurantDetail.js
+++ screens/RestaurantDetail.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import {View, Text} from "react-native";
+import {Divider} from "react-native-elements";
+import About from "../components/restaurantDetail/About";
 
 const RestaurantDetail = () => {
     return (
         <View style={{}}>
-
+            <About />
+            <Divider width={1.8} style={{marginVertical: 20}} />
         </View>
     );
 };
