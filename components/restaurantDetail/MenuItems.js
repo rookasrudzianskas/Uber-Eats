398cbedb0e896095b227b42cb2a948ae37078db3
Rokas <rookas.rudzenskas@gmail.com>
2021-10-05 17:17:36 +0300

    Rokas

--- components/restaurantDetail/MenuItems.js
+++ components/restaurantDetail/MenuItems.js
@@ -67,7 +67,7 @@ const MenuItems = () => {
                             <FoodInfo food={food} />
                             <FoodImage food={food} />
                         </View>
-                        <Divider width={0.5} orientation={'vertical'} />
+                        <Divider width={0.5} orientation={'vertical'} style={{marginHorizontal: 20}} />
                     </View>
                 ))}
         </ScrollView>
