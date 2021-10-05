f670ce4945f9c17f83c89ffc556f7f7374502f8f
Rokas <rookas.rudzenskas@gmail.com>
2021-10-05 11:27:38 +0300

    Rokas

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -1,10 +1,37 @@
 import React from 'react';
 import {View, Text} from "react-native";
+import {GooglePlacesAutocomplete} from "react-native-google-places-autocomplete";
 
 const SearchBar = () => {
     return (
-        <View>
+        <View style={{marginTop: 15, flexDirection: 'row'}}>
+            <GooglePlacesAutocomplete
+                placeholder='Search'
+                onPress={(data, details = null) => {
+                    // 'details' is provided when fetchDetails = true
+                    console.log(data, details);
+                }}
+                query={{
+                    key: 'YOUR API KEY',
+                    language: 'en',
+                }}
+                styles={{
+                    textInput: {
+                        backgroundColor: '#eee',
+                        borderRadius: 20,
+                        fontWeight: '700',
+                        marginTop: 7
+                    },
+                    textInputContainer: {
+                        backgroundColor: '#eee',
+                        borderRadius: 50,
+                        flexDirection: 'row',
+                        alignItems: 'center',
+                        marginRight: 10,
+                    },
+                }}
 
+            />
         </View>
     );
 };
