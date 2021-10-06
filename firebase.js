79490c750b508ce112acd1d381991dd891851be1
Rokas <rookas.rudzenskas@gmail.com>
2021-10-06 13:27:08 +0300

    Rokas

---
+++ firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase';
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDvauH_iySYZVXZTshFfBiiSxOUsEFIwBM",
+    authDomain: "rookas-uber-eats.firebaseapp.com",
+    projectId: "rookas-uber-eats",
+    storageBucket: "rookas-uber-eats.appspot.com",
+    messagingSenderId: "123966888385",
+    appId: "1:123966888385:web:3e4353fc479e5b915c7740",
+    measurementId: "G-F2G4ZYHB7T"
+};
+
