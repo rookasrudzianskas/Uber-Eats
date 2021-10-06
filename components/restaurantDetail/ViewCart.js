019be44012d1ef4ee42c826f9bcaafdf27e70dd6
Rokas <rookas.rudzenskas@gmail.com>
2021-10-06 11:33:11 +0300

    Rokas

--- components/restaurantDetail/ViewCart.js
+++ components/restaurantDetail/ViewCart.js
@@ -16,28 +16,37 @@ const ViewCart = () => {
 
     // console.log(totalUSD);
     return (
-        <View style={{
-            flex: 1,
-            alignItems: "center",
-            flexDirection: 'row',
-            position: 'absolute',
-            bottom: 150,
-            zIndex: 999
-        }}>
-            <View style={{flexDirection: 'row', justifyContent: 'center', width: '100%'}}>
-                <TouchableOpacity activeOpacity={0.8} style={{
-                    marginTop: 20,
-                    backgroundColor: 'black',
-                    alignItems: 'center',
-                    padding: 13,
-                    borderRadius: 30,
-                    width: 300,
-                    position: 'relative',
-                }}>
-                    <Text style={{color: 'white', fontSize: 20}}>View Cart</Text>
-                </TouchableOpacity>
+        <>
+            {total ? (
+
+
+            <View style={{
+                flex: 1,
+                alignItems: "center",
+                flexDirection: 'row',
+                position: 'absolute',
+                bottom: 150,
+                zIndex: 999
+            }}>
+                <View style={{flexDirection: 'row', justifyContent: 'center', width: '100%'}}>
+                    <TouchableOpacity activeOpacity={0.8} style={{
+                        marginTop: 20,
+                        backgroundColor: 'black',
+                        alignItems: 'center',
+                        padding: 13,
+                        borderRadius: 30,
+                        width: 300,
+                        position: 'relative',
+                    }}>
+                        <Text style={{color: 'white', fontSize: 20}}>View Cart</Text>
+                    </TouchableOpacity>
+                </View>
             </View>
-        </View>
+            ) : (
+                <>
+                </>
+            )}
+        </>
     );
 };
 
