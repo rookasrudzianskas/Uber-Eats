816dfa133f54fdf1f5f7b8f77b54f94df7f773ab
Rokas <rookas.rudzenskas@gmail.com>
2021-10-06 10:40:42 +0300

    Rokas

--- redux/reducers/cartReducer.js
+++ redux/reducers/cartReducer.js
@@ -3,7 +3,21 @@ let defaultState = {
 };
 
 let cartReducer = (state = defaultState, action) => {
+    switch (action.type) {
+        case "ADD_TO_CART": {
+            let newState = { ...state };
 
+                newState.selectedItems = {
+                    items: [...newState.selectedItems.items, action.payload],
+                    restaurantName: action.payload.restaurantName,
+                };
+
+            console.log(newState, "👉");
+            return newState;
+        }
+        default:
+            return state;
+    }
 };
 
 export default cartReducer;
